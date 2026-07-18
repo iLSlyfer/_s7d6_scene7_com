5430bc8545f2064a61e3c55ae31cc332d1f87077
Slyfer <67638136+iLSlyfer@users.noreply.github.com>
2026-07-18 12:19:58 -0300

    ♥

---
+++ 5.js
@@ -0,0 +1,15 @@
+const iframe = document.createElement("iframe");
+
+iframe.src = "https://ilslyfer.github.io/_s7d6_scene7_com/index.html";
+iframe.width = "800";
+iframe.height = "600";
+iframe.style.border = "none";
+
+// Centrar el iframe
+iframe.style.position = "fixed";
+iframe.style.top = "50%";
+iframe.style.left = "50%";
+iframe.style.transform = "translate(-50%, -50%)";
+iframe.style.zIndex = "9999";
+
+document.body.appendChild(iframe);
